be27d1fd10f3b00953bbca551240d993dc6c83bf
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-06-15 00:07:03 +0100

    Create change.js

---
+++ change.js
@@ -0,0 +1,6 @@
+/*Create 2 functions:
+    get: a function that takes a key and returns the corresponding value from the sourceObject.
+    set: a function that takes a key and a value. Update the value for the corresponding property of the sourceObject and return the value.
+ */
+    function get(key) {return sourceObject[key]};
+    function set(key, value) {return (sourceObject[key] = value)};
